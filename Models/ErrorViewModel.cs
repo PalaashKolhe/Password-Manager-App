b2a5fd068f5104b1e78a05689d557228be8a8854
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2021-10-18 22:44:25 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Password_Manager_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
