346218f485b20c33f3dbd7d4471d1f25384a869d
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2021-10-18 23:26:21 -0600

    added view passwords feature

---
+++ Data/Migrations/20211019050556_initialSetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Password_Manager_App.Data.Migrations
+{
+    public partial class initialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Password",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Platform = table.Column<string>(nullable: true),
+                    PlatformPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Password", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Password");
+        }
+    }
+}
